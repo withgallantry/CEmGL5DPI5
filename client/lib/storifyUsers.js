d83cd3ed1cb732143e5a61bedce7431d2d8902e1
Matty Gallantry <matt@wakelet.com>
2017-12-28 21:26:26 +0000

    Changes Again

--- client/lib/storifyUsers.js
+++
@@ -1,7 +0,0 @@
-export const getUsers = (page) => {
-  return new Promise((accept, reject) => {
-    fetch(`https://api.storify.com/v1/users?page=${page}&per_page=100`)
-      .then((resp) => resp.json())
-      .then((json) => accept(json));
-  });
-};
